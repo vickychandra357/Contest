cec7c239e868f813b67ab8b5f982358d56f66afb
tonystark <chandravicky288@gmail.com>
2023-03-05 00:46:11 +0530

    third

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
             x.style.display = 'block';
         }
         document.getElementById('book-name').innerHTML = searched.value;
+        let mydata=localStorage.setItem('mydata',searched.value);
         outputList.innerHTML = ""; //empty html output
         document.body.style.backgroundImage = "url('')";
         searchData = $("#search").val();
@@ -56,10 +57,11 @@ $(document).ready(function () {
     * @param response
     */
     function displayResults(response) {
-        for (var i = 0; i < response.items.length; i += 3) {
+        for (var i = 0; i < response.items.length; i +=3) {
             item = response.items[i];
             title1 = item.volumeInfo.title;
             author1 = item.volumeInfo.authors;
+            pageCount1=item.volumeInfo.pageCount;
             publisher1 = item.volumeInfo.publisher;
             bookLink1 = item.volumeInfo.previewLink;
             bookIsbn = item.volumeInfo.industryIdentifiers[1].identifier
@@ -68,6 +70,7 @@ $(document).ready(function () {
             item2 = response.items[i + 1];
             title2 = item2.volumeInfo.title;
             author2 = item2.volumeInfo.authors;
+            pageCount2=item.volumeInfo.pageCount;
             publisher2 = item2.volumeInfo.publisher;
             bookLink2 = item2.volumeInfo.previewLink;
             bookIsbn2 = item2.volumeInfo.industryIdentifiers[1].identifier
@@ -76,8 +79,8 @@ $(document).ready(function () {
 
             // in production code, item.text should have the HTML entities escaped.
             outputList.innerHTML += '<div class="row mt-4">' +
-                formatOutput(bookImg1, title1, author1, publisher1, bookLink1, bookIsbn) +
-                formatOutput(bookImg2, title2, author2, publisher2, bookLink2, bookIsbn2) +
+                formatOutput(bookImg1, title1, author1, publisher1, pageCount1, bookIsbn) +
+                formatOutput(bookImg2, title2, author2, publisher2, pageCount2, bookIsbn2) +
                 '</div>';
 
             console.log(outputList);
@@ -89,7 +92,7 @@ $(document).ready(function () {
     * @param bookImg title author publisher bookLink
     * @return htmlCard
     */
-    function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
+    function formatOutput(bookImg, title, author, publisher, pageCount, bookIsbn) {
         // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
         var viewUrl = 'index.html?isbn=' + bookIsbn; //constructing link for bookviewer
         var htmlCard = `<div class="col-lg-6">
@@ -103,7 +106,8 @@ $(document).ready(function () {
                  <h5 class="card-title">${title}</h5>
                  <p class="card-text">Author: ${author}</p>
                  <p class="card-text">Publisher: ${publisher}</p>
-                 <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
+                 <p class="card-text">PageCount: ${pageCount}</p>
+                 <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Buy Book</a>
                </div>
              </div>
            </div>
@@ -120,108 +124,16 @@ $(document).ready(function () {
        
 
 
-    //Search History
-
-
-    //     if (localStorage.getItem('itemJson') == null) {
-    //         itemJsonArray = [];
-    //         itemJsonArray
-    //             .push([searched.value, datetime]);
-    //         localStorage.setItem('itemJeson',
-    //             JSON.stringify(itemJsonArray))
-    //     }
-    //     else {
-    //         itemJsonArrayStr=
-    //         localStorage.getItem('itemJeson');
-    //         itemJsonArry = JSON.parse(
-    //             itemJsonArrayStr);
-
-    //         itemJsonArray
-    //             .push([searched.value, datetime]);
-    //         localStorage.setItem('itemJeson',
-    //             JSON.stringify(itemJsonArray))
-    //     }
-
-    //     update();
-    //     // updating inputs in table   
-    //     function update() {
-    //         if (localStorage.getItem('itemJson') == null) {
-    //             itemJsonArray = [];
-    //             localStorage.setItem('itemJeson',
-    //                 JSON.stringify(itemJsonArray))
-    //         }
-    //         else {
-    //             itemJsonArrayStr =
-    //                 localStorage.getItem('itemJeson');
-    //             itemJsonArry = JSON.parse(
-    //                 itemJsonArrayStr);
-    //         }
-    //         let tableData = document.getElementById("tabledata");
-    //         let str = "";
-    //         itemJsonArray.forEach((element, index) => {
-
-    //             str += ` <tr>
-    // <th scope="row"> ${index}</th>
-    //  <td> ${element[0]}</td>
-    //   <td> ${element[1]}</td>
-    //             </tr>`;
-    //         });
-    //         tablebody.innerHTML = str;
-
-    //     }
-    //     update();
-
-
-    //     //Clear history
-
-    // let clearHistory;
-    // function clearHistory1() {
-    //   clearHistory = arr.splice(index, 1);
-    //   console.log(clearHistory);
-    //   // Now after removing the specified indexed element reprint the all user again
-    //   tableBodyContent.innerHTML = '';
-    //   console.log(arr)
-    //   arr.map((element, index) => {
-    //     tableBodyContent.innerHTML += `
-    //     <tr>
-    //     <td>id: ${index + 1}.</td>
-    //     <td>Name: ${element.searched.value}</td>
-    //     <td>Prefession: ${element.datetime}</td>
-    //     </tr>`
-    //     console.log(tableBodyContent.innerHTML);
-    //   })
-    //   console.log(arr)
-    //   if (arr.length === 0) {
-    //     document.getElementById('table').classList.add('hidden');
-    //   }
-    // }
-
+   
 });
 
 function history(){
     window.location.href ="/history.html";
-    let date = new Date();
-    let datetime = "Searched On" + date.getDate() + "at" + currentdate.getHours() + ":"
-        + currentdate.getMinutes() + "PM";
-    let inputText=searchData.value;
-    if(!inputText){
-        var searchHistoryData={
-            inputText:inputText,
-            datetime:datetime,
-        };
-        const searchHistory=localStorage.getItem("books");
-        console.log("Books",books);
-        var booksArr=[];
-        if(books){
-            booksArr=JSON.parse(localStorage.getItem("books"));
-        }
-        else{
-            booksArr=[searchHistoryData];
-        }
-       let tableData=document.getElementById('tableBody').innerHTML= localStorage.setItem("books", JSON.stringify(booksArr));
-        window.location.href ="/searched.html";
-    } 
-}
-
+    const date = new Date();
+    // let datetime = "Searched On" + date.getDate() + "at" + date.getHours() + ":"
+    //     + date.getMinutes() + "PM"
+    let mydate=localStorage.setItem('mydate',date)
+    document.getElementById('tableBody').innerHTML=localStorage.getItem('mydata');
+      }
 
 
